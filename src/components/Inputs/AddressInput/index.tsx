062c80804e34e95e9971167381a5727d061d26c0
Beatriz Cavallieri <bfcavallieri@gmail.com>
2023-04-19 15:18:13 -0300

    Feature: searches address details from CEP number

--- src/components/Inputs/AddressInput/index.tsx
+++ src/components/Inputs/AddressInput/index.tsx
@@ -1,7 +1,9 @@
+import axios from 'axios';
 import { InputSC, LabelSC } from '../../../pages/CreateContact/styles';
 import { Address } from '../../../types/address';
-import { RemoveButtonSC } from '../../Button';
+import { Button, RemoveButtonSC } from '../../Button';
 import { AddressInputSC } from './styles';
+import { useState } from 'react';
 
 interface IAddressInput extends Address {
     onChange: (address: Address) => void;
@@ -9,12 +11,44 @@ interface IAddressInput extends Address {
     index: number;
 }
 
+interface ICorreios {
+    logradouro: string;
+    bairro: string;
+    localidade: string;
+    uf: string;
+}
+
 const AddressInput = (props: IAddressInput) => {
-    const { index, street, number, neighborhood, city, state, onChange, onRemove } = props;
+    const { index, number, complement, onChange, onRemove } = props;
+
+    const [state, setState] = useState(props.state)
+    const [cep, setCep] = useState(props.cep)
+    const [neighborhood, setNeighborhood] = useState(props.neighborhood)
+    const [city, setCity] = useState(props.city)
+    const [street, setStreet] = useState(props.street)
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
         onChange({ ...props, [name]: value });
+    };
+
+    const updateAddressFromCep = (data: ICorreios) => {
+        setState(data.uf)
+        setCity(data.localidade)
+        setNeighborhood(data.bairro)
+        setStreet(data.logradouro)
+    }
+
+    const findAddress = async () => {
+        try {
+            const response = await axios.get<ICorreios>(
+                `https://viacep.com.br/ws/${cep}/json/`
+            );
+            updateAddressFromCep(response.data)
+
+        } catch (error) {
+            console.log(error);
+        }
     };
 
     return (
@@ -24,14 +58,30 @@ const AddressInput = (props: IAddressInput) => {
                 {index > 0 && <RemoveButtonSC
                     aria-label='Remover Endereço'
                     type="button"
-                    onClick={props.onRemove}>
+                    onClick={onRemove}>
                     x
                 </RemoveButtonSC>
                 }
             </div>
+            <div style={{ display: 'flex' }}>
+                <LabelSC htmlFor={`cep-${index}`}>
+                    CEP:*
+                    <InputSC
+                        required
+                        type="text"
+                        id={`cep-${index}`}
+                        value={cep}
+                        onChange={(event) => setCep(event.target.value)}
+                    />
+                </LabelSC>
+                <Button type="button" onClick={findAddress}>
+                    Buscar endereço
+                </Button>
+            </div>
             <div>
-                <LabelSC htmlFor={`street-${index}`}>Rua:</LabelSC>
+                <LabelSC htmlFor={`street-${index}`}>Rua:*</LabelSC>
                 <InputSC
+                    required
                     type="text"
                     id={`street-${index}`}
                     name="street"
@@ -39,8 +89,9 @@ const AddressInput = (props: IAddressInput) => {
                     onChange={handleInputChange} />
             </div>
             <div>
-                <LabelSC htmlFor={`number-${index}`}>Número:</LabelSC>
+                <LabelSC htmlFor={`number-${index}`}>Número:*</LabelSC>
                 <InputSC
+                    required
                     type="text"
                     id={`number-${index}`}
                     name="number"
@@ -48,8 +99,28 @@ const AddressInput = (props: IAddressInput) => {
                     onChange={handleInputChange} />
             </div>
             <div>
-                <LabelSC htmlFor={`city-${index}`}>Cidade:</LabelSC>
+                <LabelSC htmlFor={`complement-${index}`}>Complemento:</LabelSC>
                 <InputSC
+                    type="text"
+                    id={`complement-${index}`}
+                    name="complement"
+                    value={complement}
+                    onChange={handleInputChange} />
+            </div>
+            <div>
+                <LabelSC htmlFor={`neighborhood-${index}`}>Bairro:*</LabelSC>
+                <InputSC
+                    required
+                    type="text"
+                    id={`neighborhood-${index}`}
+                    name="neighborhood"
+                    value={neighborhood}
+                    onChange={handleInputChange} />
+            </div>
+            <div>
+                <LabelSC htmlFor={`city-${index}`}>Cidade:*</LabelSC>
+                <InputSC
+                    required
                     type="text"
                     id={`city-${index}`}
                     name="city"
@@ -57,8 +128,9 @@ const AddressInput = (props: IAddressInput) => {
                     onChange={handleInputChange} />
             </div>
             <div>
-                <LabelSC htmlFor={`state-${index}`}>Estado:</LabelSC>
+                <LabelSC htmlFor={`state-${index}`}>Estado:*</LabelSC>
                 <InputSC
+                    required
                     type="text"
                     id={`state-${index}`}
                     name="state"
