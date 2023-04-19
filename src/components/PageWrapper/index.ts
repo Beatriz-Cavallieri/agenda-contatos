70d773a35a26e618e237f8b5342c98eec37593e8
Beatriz Cavallieri <bfcavallieri@gmail.com>
2023-04-19 18:41:55 -0300

    Feature: search

--- src/components/PageWrapper/index.ts
+++ src/components/PageWrapper/index.ts
@@ -3,6 +3,6 @@ import styled from "styled-components";
 export const PageWrapper = styled.div`
   margin: 2rem;
   height: 100%;
-  width: 100vw;
+  width: 90vw;
   text-align: left;
 `;
--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -6,10 +6,21 @@ import { Link } from 'react-router-dom';
 import { ColorButton } from '../../components/Button';
 import { PageWrapper } from '../../components/PageWrapper';
 import { Header1 } from '../../components/Headers';
+import { InputSC } from '../CreateContact/styles';
 
 
 const Homepage = () => {
     const [contacts, setContacts] = useState<Array<IContact>>([])
+    const [searchText, setSearchText] = useState("")
+
+    const filterContacts = () => {
+        return contacts
+            .filter((contact) => {
+                const name = contact.name.toLowerCase();
+                const term = searchText.toLowerCase();
+                return name.includes(term);
+            })
+    }
 
     useEffect(() => {
         async function loadContacts(): Promise<void> {
@@ -20,13 +31,21 @@ const Homepage = () => {
     }, []);
     return (
         <PageWrapper>
-            <Header1>Contatos</Header1>
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                <Header1>Contatos</Header1>
+                <InputSC
+                    type="text"
+                    placeholder="Buscar contato"
+                    value={searchText}
+                    onChange={(e) => setSearchText(e.target.value)}
+                />
+            </div>
             <Link to={'/create'}>
                 <ColorButton>
                     + Criar contato
                 </ColorButton>
             </Link>
-            {contacts && contacts.map(contact => <ContactInfo {...contact} />)}
+            {contacts && filterContacts().map(contact => <ContactInfo {...contact} />)}
         </PageWrapper>
     )
 }
